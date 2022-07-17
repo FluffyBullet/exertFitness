8491344b751ff6f08ffbe0f7d3e8a39b5d8f6aee
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2022-07-17 20:13:22 +0100

    Addition of greeting banner variable on time of day

--- assets/JavaScript/data.js
+++ assets/JavaScript/data.js
@@ -3,3 +3,14 @@ let press = document.getElementById('trial');
 press.addEventListener("click", function(){
     alert("This JS Script is working!");
 })
+
+let date = new Date();
+let hour = date.getHours();
+let greetMessage = document.getElementById('greet');
+if (hour < 11){
+    greet.innerHTML = "Good Morning!";
+} else if (hour < 17){
+    greet.innerHTML = "Good Afternoon!";
+} else {
+    greet.innerHTML = "Good Evening!";
+}
