c070b189f5feff84c465bdc161c043e016146b3a
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2022-08-01 19:41:22 +0100

    removal of excess code and commented out code, i.e variables which are called by button pressing

--- assets/javascript/data2.js
+++ assets/javascript/data2.js
@@ -1,20 +1,17 @@
-let stageTwo = document.getElementById('dataEntry');
-let buttonOne = document.getElementById('stepOne');
 google.charts.load(current, {packages:['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-
-
-/** Starting calculations from first entry fields */
+// Starting calculations from first entry fields */
 function stepOne(){
-    /**Declaring variables for calculations */
+    //Declaring variables for calculations /
+let stageTwo = document.getElementById('dataEntry');
 let name = document.getElementById('name').value;
 let tCal = parseInt(document.getElementById('target').value);
 let date = document.getElementById('date').value;
 let meals = parseInt(document.getElementById('meals').value);
 let percent5,percent10,percent15,percent20,percent25,percent30,percent35,percent40;
 
-/** Calculating values*/
+// Calculating values/
     percent5  = tCal*0.05;
     percent10 = tCal*0.1;
     percent15 = tCal*0.15;
@@ -114,13 +111,15 @@ let percent5,percent10,percent15,percent20,percent25,percent30,percent35,percent
                 <td><input id="dinner"></td>
                 <td>${percent25} / ${percent30}</td>
             </tr>
-        </table>`;
+        </table>
+        <button onclick="drawChart()">Display Chart</button>`;
+        
     } else {
         stageTwo.innerHTML = `
         <p>:( Something went wrong!</p> `;
     }
 
-        /** Updating of "next" button on calculator page */
+        // Updating of "next" button on calculator page /
 
         let next = document.getElementById('stepOne');
         next.innerText = "Update"
@@ -140,7 +139,7 @@ function myFunction() {
  */
 
 
-/* create of pie chart, taken from w3 Schools*/
+// create of pie chart, taken from w3 Schools/
 
 function drawChart(){
     let meals = parseInt(document.getElementById('meals').value);
@@ -150,7 +149,7 @@ function drawChart(){
     let morningSnack;
     let afternoonSnack;
 
-    let totalValue = totalValue = parseInt(breakfast.value) + parseInt(lunch.value) + parseInt(dinner.value);;
+    let totalValue = parseInt(breakfast.value) + parseInt(lunch.value) + parseInt(dinner.value);;
     if (meals == 4){ 
         morningSnack = document.getElementById('morningSnack');
         totalValue = parseInt(breakfast.value) + parseInt(morningSnack.value) + parseInt(lunch.value) + parseInt(dinner.value);
@@ -163,7 +162,6 @@ function drawChart(){
     console.log(morningSnack);
     console.log(typeof(afternoonSnack));
 
-    // console.log(totalValue);
     const barColors = [
         "#FCF5F0",
         "#F5D8BB",
